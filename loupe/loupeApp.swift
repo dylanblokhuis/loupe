83193ba66b30e7a848cd4be4f4b6a56030315329
Dylan Blokhuis <dylanblokhuis3@hotmail.com>
2026-08-28 11:39:21 +0200

    Initial Commit

---
+++ loupe/loupeApp.swift
@@ -0,0 +1,17 @@
+//
+//  loupeApp.swift
+//  loupe
+//
+//  Created by Dylan Blokhuis on 28/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct loupeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
